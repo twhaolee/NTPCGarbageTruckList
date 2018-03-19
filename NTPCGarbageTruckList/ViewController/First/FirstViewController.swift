ea3c8a89a38b08aa7923bbdf4c9483d8cc2a8cd2
Hao Lee <twhaolee@icloud.com>
2018-03-19 21:22:22 +0800

    Create first view controller

---
+++ NTPCGarbageTruckList/ViewController/First/FirstViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FirstViewController.swift
+//  NTPCGarbageTruckList
+//
+//  Created by Hao Lee on 2018/3/19.
+//
+
+import UIKit
+
+class FirstViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
