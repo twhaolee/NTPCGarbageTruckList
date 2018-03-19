603ad76bad98a747aab512541d16b03876678fca
Hao Lee <twhaolee@icloud.com>
2018-03-20 00:06:41 +0800

    Design HTTP get base method

--- NTPCGarbageTruckList/Network/NTPCOpenDataConnect.swift
+++ NTPCGarbageTruckList/Network/NTPCOpenDataConnect.swift
@@ -12,4 +12,30 @@ class NTPCOpenDataConnect: NSObject {
 
     let serverUrl: URL = URL(string: "http://data.ntpc.gov.tw")!
     let apiRootPaths: [String] = ["od/data/api"]
+
+    private func getData<T: Codable>(apiPath: String,
+                                     queryItems: [URLQueryItem],
+                                     completionHandler: @escaping (T?, Error?) -> Void) {
+        var url = self.serverUrl.appendingPathComponent(apiPath)
+
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        components.queryItems = queryItems
+
+        url = components.url ?? url
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        let taskCompletionHandler = { (data: Data?, response: URLResponse?, error: Error?) in
+
+        }
+
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            DispatchQueue.main.async {
+                taskCompletionHandler(data, response, error)
+            }
+        })
+
+        task.resume()
+    }
 }
