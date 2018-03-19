1e1b80072ef83d61ace16e32a006f179340cd693
Hao Lee <twhaolee@icloud.com>
2018-03-20 00:00:22 +0800

    Setup data ID for New Taipei City open data

---
+++ NTPCGarbageTruckList/Network/NTPCApiDataType.swift
@@ -0,0 +1,13 @@
+//
+//  NTPCApiDataType.swift
+//  NTPCGarbageTruckList
+//
+//  Created by Hao Lee on 2018/3/19.
+//
+
+import Foundation
+
+enum NTPCApiDataType: String {
+    case zipcode = "AC110AF8-C847-43E5-B62C-7985E9E049F9"
+    case garbageTruckInfo = "EDC3AD26-8AE7-4916-A00B-BC6048D19BF8"
+}
