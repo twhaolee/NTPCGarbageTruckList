471107dd72cce5ac30de48f2f278eb1860367bde
Hao Lee <twhaolee@icloud.com>
2018-03-21 01:45:07 +0800

    Define connection status

---
+++ NTPCGarbageTruckList/Extension/ConnectionStatus.swift
@@ -0,0 +1,16 @@
+//
+//  ConnectionStatus.swift
+//  NTPCGarbageTruckList
+//
+//  Created by Hao Lee on 2018/3/21.
+//
+
+import Foundation
+
+enum ConnectionStatus {
+    /// 閒置
+    case idle
+
+    /// 連線中
+    case connecting
+}
