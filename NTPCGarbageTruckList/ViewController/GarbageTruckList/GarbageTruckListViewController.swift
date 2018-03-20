9a6d55e8ddfe7d41a9fb9d325af68c9f2425dbf4
Hao Lee <twhaolee@icloud.com>
2018-03-21 00:42:51 +0800

    Preview table view cell on garbage truck list view controller

--- NTPCGarbageTruckList/ViewController/GarbageTruckList/GarbageTruckListViewController.swift
+++ NTPCGarbageTruckList/ViewController/GarbageTruckList/GarbageTruckListViewController.swift
@@ -23,3 +23,20 @@ class GarbageTruckListViewController: ViewController {
     }
 
 }
+
+extension GarbageTruckListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(for: indexPath) as GarbageTruckTableViewCell
+
+        return cell
+    }
+
+}
